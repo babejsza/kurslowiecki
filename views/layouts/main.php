5a40c1e9ffde537314df003f6de9f52d5af53d6a
babejsza <babejsza@gmail.com>
2016-05-16 14:47:43 +0200

    livechat

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -89,6 +89,19 @@ AppAsset::register($this);
 </footer>
 
 <?php $this->endBody() ?>
+
+<!-- Start of LiveChat (www.livechatinc.com) code -->
+<script type="text/javascript">
+    window.__lc = window.__lc || {};
+    window.__lc.license = 7513391;
+    (function() {
+        var lc = document.createElement('script'); lc.type = 'text/javascript'; lc.async = true;
+        lc.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'cdn.livechatinc.com/tracking.js';
+        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(lc, s);
+    })();
+</script>
+<!-- End of LiveChat code -->
+
 </body>
 </html>
 <?php $this->endPage() ?>
